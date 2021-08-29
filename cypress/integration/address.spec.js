9a1ecaf8d0b3b618faa7f6167066cdfa4b357060
Gerda Monkeviciute <gerda.monkeviciute@juvare.com>
2021-08-29 17:47:05 +0300

    Implement POM

--- cypress/integration/address.spec.js
+++ cypress/integration/address.spec.js
@@ -1,13 +1,42 @@
 import ApiRequests from '../utils/apiRequests.utils';
+import AddressPage from '../page-objects/address/addressPage';
 
 const apiRequests = new ApiRequests();
 
 const addressName = `Cypress random address name ${Math.floor(Math.random() * 100000000)}`;
+const addressDetails = {
+    country: 'Country',
+    fullName: addressName,
+    mobileNum: 1234567890,
+    zipCode: '12345',
+    streetAddress: 'Address',
+    city: 'City',
+    state: 'State',
+};
+const expectedNewAddressDetailsInTable = {
+    fullName: addressDetails.fullName,
+    streetAddress: `${addressDetails.streetAddress}, ${addressDetails.city}, ${addressDetails.state}, ${addressDetails.zipCode}`,
+    country: addressDetails.country,
+};
+const editAddressDetails = {
+    country: 'Country2',
+    fullName: `2${addressName}`,
+    mobileNum: 1234567891,
+    zipCode: '12346',
+    streetAddress: 'Address2',
+    city: 'City2',
+    state: 'State2',
+};
+const expectedEditedAddressDetailsInTable = {
+    fullName: editAddressDetails.fullName,
+    streetAddress: `${editAddressDetails.streetAddress}, ${editAddressDetails.city}, ${editAddressDetails.state}, ${editAddressDetails.zipCode}`,
+    country: editAddressDetails.country,
+};
 
 describe('Address Page tests', () => {
     before(() => {
         cy.login(Cypress.env('email'), Cypress.env('password'));
-        cy.visit('/#/address/saved');
+        AddressPage.openAddressPage();
     });
 
     beforeEach(() => {
@@ -16,76 +45,24 @@ describe('Address Page tests', () => {
     });
 
     it('should be able to create new address', () => {
-        cy.get('[data-cy="add-new-address-button"]').click();
+        AddressPage.openNewAddressForm();
+        AddressPage.addressForm.fillInAddress(addressDetails);
+        AddressPage.addressForm.submitAddressForm();
 
-        cy.get('[data-cy="country-input"]').type('Country');
-        cy.get('[data-cy="name-input"]').type(addressName);
-        cy.get('[data-cy="mobile-number-input"]').type('1234567890');
-        cy.get('[data-cy="zip-code-input"]').type('12345');
-        cy.get('[data-cy="address-textarea"]').type('Address');
-        cy.get('[data-cy="city-input"]').type('City');
-        cy.get('[data-cy="state-input"]').type('State');
-        cy.get('[data-cy="submit-button"]').click();
-
-        cy.get('[data-cy="addresses-table"] mat-row')
-            .last()
-            .within(() => {
-                cy.get('[data-cy="full-name"]').should('contain.text', addressName);
-                cy.get('[data-cy="address"]').should('contain.text', 'Address, City, State, 12345');
-                cy.get('[data-cy="country"]').should('contain.text', 'Country');
-                cy.get('[data-cy="edit-button"]').should('be.visible');
-                cy.get('[data-cy="delete-button"]').should('be.visible');
-            });
+        AddressPage.assertAddressTableLastRow(expectedNewAddressDetailsInTable);
     });
 
     it('should be able to edit existing address', () => {
-        const addressDetails = {
-            country: 'Country',
-            fullName: addressName,
-            mobileNum: 1234567890,
-            zipCode: '12345',
-            streetAddress: 'Address',
-            city: 'City',
-            state: 'State',
-        };
-
         apiRequests.createAddress(addressDetails);
 
         cy.reload();
-        cy.get('[data-cy="addresses-table"] mat-row')
-            .last()
-            .within(() => {
-                cy.get('[data-cy="full-name"]').should('contain.text', addressName);
-                cy.get('[data-cy="address"]').should('contain.text', 'Address, City, State, 12345');
-                cy.get('[data-cy="country"]').should('contain.text', 'Country');
-                cy.get('[data-cy="edit-button"]').should('be.visible');
-                cy.get('[data-cy="delete-button"]').should('be.visible');
-            });
+        AddressPage.assertAddressTableLastRow(expectedNewAddressDetailsInTable);
 
-        cy.get('[data-cy="addresses-table"] mat-row')
-            .last()
-            .within(() => {
-                cy.get('[data-cy="edit-button"]').click();
-            });
+        AddressPage.openLastAddressForEdit();
+        AddressPage.addressForm.fillInAddress(editAddressDetails);
+        AddressPage.addressForm.submitAddressForm();
 
-        cy.get('[data-cy="country-input"]').clear().type('Country2');
-        cy.get('[data-cy="name-input"]').clear().type(`2${addressName}`);
-        cy.get('[data-cy="mobile-number-input"]').clear().type('2234567890');
-        cy.get('[data-cy="zip-code-input"]').clear().type('22345');
-        cy.get('[data-cy="address-textarea"]').clear().type('Address2');
-        cy.get('[data-cy="city-input"]').clear().type('City2');
-        cy.get('[data-cy="state-input"]').clear().type('State2');
-        cy.get('[data-cy="submit-button"]').click();
-
-        cy.get('[data-cy="addresses-table"] mat-row')
-            .last()
-            .within(() => {
-                cy.get('[data-cy="full-name"]').should('contain.text', `2${addressName}`);
-                cy.get('[data-cy="address"]').should('contain.text', 'Address2, City2, State2, 22345');
-                cy.get('[data-cy="country"]').should('contain.text', 'Country2');
-                cy.get('[data-cy="edit-button"]').should('be.visible');
-                cy.get('[data-cy="delete-button"]').should('be.visible');
-            });
+        AddressPage.assertAddressTableLastRow(expectedEditedAddressDetailsInTable);
     });
 
     afterEach(() => {
