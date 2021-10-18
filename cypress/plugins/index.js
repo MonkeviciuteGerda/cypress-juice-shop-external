3c8833bb4f68b7fb401113cbf5ca0c351c1e79cd
Gerda Monkeviciute <gerda.monkeviciute@juvare.com>
2021-10-18 10:49:41 +0300

    Add config for multiple environments

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,7 +16,15 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+const fs = require('fs-extra');
+const path = require('path');
+function getConfigurationByFile(file) {
+    const pathToConfigFile = path.resolve('cypress', 'config', `cypress.${file}.json`);
+    return fs.readJson(pathToConfigFile);
 }
+
+module.exports = (on, config) => {
+    // accept a configFile value or use qa by default
+    const file = config.env.configFile || 'qa';
+    return getConfigurationByFile(file);
+};
